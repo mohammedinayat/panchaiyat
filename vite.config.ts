c6ed1af8d86ed609d70c6a100e5628af6ca83859
mohammedinayat <mohammedinayat8bp@gmail.com>
2026-05-27 19:49:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
     react(),
   ],
-  // Required for GitHub Pages — replace 'panchaiyat' with your repo name
-  base: "/panchaiyat/",
+  base: "/",
   build: {
     outDir: "dist",
   },
